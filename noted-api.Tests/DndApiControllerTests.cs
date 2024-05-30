3a8b5455e4337a8dcc9cc3140c7572a4ae22304d
Denzel Huijbers <huijbers15@outlook.com>
2024-05-30 10:56:23 +0200

    :(

---
+++ noted-api.Tests/DndApiControllerTests.cs
@@ -0,0 +1,20 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+using noted_dndapi_service.Controllers;
+
+namespace noted_dndapi_service.Tests
+{
+    public class DndApiControllerTests
+    {
+        [Fact]
+        public async Task Example_Test()
+        {
+           var x = 1;
+            
+            Assert.Equal(x, 2);
+        }
+    }
+}
