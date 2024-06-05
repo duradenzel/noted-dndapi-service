941beaf9bac07082d5697f79542c2cc66fa8c7dc
Denzel Huijbers <huijbers15@outlook.com>
2024-06-05 15:42:00 +0200

    build script coverlet test

--- noted-dndapi-service/obj/Debug/net7.0/noted-dndapi-service.AssemblyInfo.cs
+++ noted-dndapi-service/obj/Debug/net7.0/noted-dndapi-service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("noted-dndapi-service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+535d2dc5a72c26a894d459a0f476644b80bd2f33")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a904679fd15656735c4dc35c53ea47b4eda0165")]
 [assembly: System.Reflection.AssemblyProductAttribute("noted-dndapi-service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("noted-dndapi-service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
